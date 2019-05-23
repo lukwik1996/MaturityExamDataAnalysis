40e9080525e97af24e387f4ce908a44a55ab8623
lukwik1996 <lukwik1996@github.com>
2019-05-23 21:42:47 +0200

    Almost completed, empty test script.

---
+++ MaturityExamAnalysisTests.py
@@ -0,0 +1,12 @@
+import MaturityExamAnalysis
+try:
+    import pytest
+except ImportError:
+    print("You need pytest module intalled to run the tests. "
+          "Visit https://docs.pytest.org/en/latest/getting-started.html for installation help.")
+
+
+if __name__ == '__main__':
+
+    print("Congrats, you have pytest installed.")
+    # tests
